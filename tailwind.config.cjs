43b42b543373500c364869a46d3419bcd3aa7fff
Revansa Helsa Kuswana <revansakuswana@gmail.com>
2025-01-28 00:18:13 +0700

    fix: adding plugins

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,16 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.jsx",
+import plugin from 'tailwindcss/plugin';
+
+export default {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'], // Sesuaikan dengan lokasi file Anda
+  theme: {},
+  plugins: [
+    plugin(({ addUtilities }) => {
+      addUtilities({
+        '.bg-radial-purple-black': {
+          backgroundImage:
+            'linear-gradient(-225deg, #3D4E81 0%, #5753C9 48%, #6E7FF3 100%)',
+        },
+      });
+    }),
   ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+};
